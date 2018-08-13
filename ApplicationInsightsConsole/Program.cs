e6502675ef999b6af12299422d76e59d4a242368
Poy Chang <Poy_Chang@kingston.com.tw>
2018-08-13 08:49:48 +0800

    Add project files.

---
+++ ApplicationInsightsConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApplicationInsightsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
